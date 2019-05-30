3532d166e77d43723a47eed70656d1b3aaa33832
yxy <15355462400@163.com>
2019-05-30 20:16:53 +0800

    add banner

--- src/components/home/Home.vue
+++ src/components/home/Home.vue
@@ -1,17 +1,34 @@
 <template>
 <div>
-  <h1>{{welcome}}</h1>
+  <!--轮播组件-->
+  <Banner/>
+  <!--搜索区-->
+  <Search/>
+  <!-- 导航 -->
+  <Nav/>
+  <!-- 展示 -->
+  <List />
 </div>
 </template>
 
 <script>
+  import Banner from "./Banner";
+  import List from "./Banner";
+  import Nav from "./Banner";
+  import Search from "./Banner";
     export default {
         name: "Home",
         data:function () {
           return{
             welcome:"Welcome to Home"
           };
-        }
+        },
+      components:{
+          Banner,
+          List,
+          Nav,
+          Search
+      }
     }
 </script>
 
